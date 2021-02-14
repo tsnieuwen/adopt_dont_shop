8266e91d91fc93cf5a2844acb76928bc4b958089
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-02-13 19:18:08 -0700

    Complete tests and code for application show page

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,10 +2,4 @@ class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
 
-  # def list_pets_on_application
-  #   pets.map do |pet|
-  #     pet.name
-  #   end
-  # end
-
 end
--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -2,15 +2,33 @@ require 'rails_helper'
 
 RSpec.describe "As a visitor" do
   before :each do
+
+    @shelter1 = Shelter.create(name: "Denver Humane Society",
+                              address: "123 Main Street",
+                              city: "Denver",
+                              state: "CO",
+                              id: 1,
+                              zip: 12588)
+
+    @pet1 = Pet.create(image: "rudy.jpg",
+                      name: "Rudy",
+                      approximate_age: 1,
+                      shelter_id: 1,
+                      description: "Full of energy",
+                      adoptable: false,
+                      sex: 1,
+                      id: 1)
+
     @application1 = Application.create(name: "Tommy",
                                       address: "2860 W32 Ave",
                                       city: "Denver",
                                       state: "CO",
                                       zip: 80211,
                                       description: "I like dogs",
-                                      pet_names: "Fido, Rudy, Spot",
                                       application_status: "Pending",
                                       id: 1)
+
+    PetApplication.create!(pet: @pet1, application: @application1)
   end
   describe "When I visit an applications show page" do
     it "Renders the applicant's name" do
@@ -33,15 +51,25 @@ RSpec.describe "As a visitor" do
 
     it "Renders names of pets that the applicant want to adopt" do
       visit '/applications/1'
-      expect(page).to have_content(@application1.pet_names)
+      expect(page).to have_content(@pet1.name)
     end
 
     it "Renders the application status" do
       visit '/applications/1'
       expect(page).to have_content(@application1.application_status)
     end
-    
+
+    it "Renders the name of the pets as a hyperlink" do
+      visit '/applications/1'
+      expect(page).to have_link(@pet1.name)
+    end
+
+    describe "When I click on a pet name" do
+      it "Takes me to the pet's show page" do
+        visit '/applications/1'
+        click_link("#{@pet1.name}")
+        expect(page).to have_current_path("/pets/#{@pet1.id}")
+      end
+    end
   end
-
-
 end
