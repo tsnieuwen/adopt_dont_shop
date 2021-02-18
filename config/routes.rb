becb9d62e5de94d6f14459e7470ce41507f686ca
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-02-17 20:05:34 -0700

    Complete code for stories 11, 12, and 13

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,10 @@ Rails.application.routes.draw do
   get "/applications/new", to: "applications#new"
   post "applications", to: "applications#create"
   get "/applications/:id", to: "applications#show"
-  patch '/applications/:id', to: 'applications#update'
+  patch '/applications/:id', to: "applications#update"
 
   post "/pet_applications", to: "pet_applications#create"
+  patch "/pet_applications", to: "pet_applications#update"
+
+  get "admin/applications/:id", to: "applications#admin_show"
 end
